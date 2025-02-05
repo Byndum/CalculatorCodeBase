08699ee0320a79b09a87d3f0cbe9a3fe628cf29d
Byndum <Mikael.List-Jensen@hotmail.dk>
2025-02-05 18:47:46 +0100

    copied over tests from simplecalc to cachedcalc

--- Tests/CachedCalculatorTest.cs
+++ Tests/CachedCalculatorTest.cs
@@ -18,4 +18,123 @@ public class CachedCalculatorTest
         // Assert
         Assert.That(result, Is.EqualTo(5));
     }
+    [Test]
+    public void Subtract()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 5;
+        var b = 3;
+
+        // Act
+        var result = calc.Subtract(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+    [Test]
+    public void Multiply()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 5;
+        var b = 2;
+
+        // Act
+        var result = calc.Multiply(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(10));
+    }
+    [Test]
+    public void Divide()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 10;
+        var b = 2;
+
+        // Act
+        var result = calc.Divide(a, b);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(5));
+    }
+    [Test]
+    public void Factorial_ValidPossitiveInt_FactorialOfNumber()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 11;
+
+        // Act
+        var result = calc.Factorial(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(39916800));
+    }
+    [Test]
+    public void Factorial_Integer0_Returns1()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 0;
+
+        // Act
+        var result = calc.Factorial(a);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(1));
+    }
+    [Test]
+    public void Factorial_InvalidNegativeInt_ThrowsArgumentException()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var invalidVal = -1;
+
+        // Act + Assert
+        Assert.Throws<ArgumentException>(
+            () => calc.Factorial(invalidVal)
+        );
+    }
+    [Test]
+    public void IsPrime_ValidPossitiveInt_IsPrime()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 7;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+    [Test]
+    public void IsPrime_ValidPossitiveInt_IsNotPrime()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 4;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+    [Test]
+    public void IsPrime_Integer1_IsNotPrime()
+    {
+        // Arrange
+        var calc = new SimpleCalculator();
+        var a = 1;
+
+        // Act
+        var result = calc.IsPrime(a);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
 }
