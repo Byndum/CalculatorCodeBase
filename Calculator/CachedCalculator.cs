a0a748b82d263a4f8e2565934f0664b91d6550b0
Byndum <Mikael.List-Jensen@hotmail.dk>
2025-02-18 12:46:54 +0100

    readonly

--- Calculator/CachedCalculator.cs
+++ Calculator/CachedCalculator.cs
@@ -5,7 +5,7 @@ namespace Calculator;
 public class CachedCalculator : ICalculator
 {
     public readonly SimpleCalculator _calculator = new();
-    private Dictionary<string, Calculation> _cache = new();
+    private readonly Dictionary<string, Calculation> _cache = new();
     public IReadOnlyDictionary<string, Calculation> Cache => _cache;
     
     public int Add(int a, int b)
